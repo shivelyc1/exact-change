4fcf3762be9d6a5fd7329e1e84e79dbfe17012ed
Carson Shively <cshively.business@gmail.com>
2024-11-14 18:35:37 -0600

    enhanced comments

--- exact_change.py
+++ exact_change.py
@@ -1,28 +1,33 @@
 from Enum.denomination_values import DenominationValues
 from math import floor
 
-def CalcDenominationQuantity(dollarAmount: float, currencyValue: DenominationValues ):
-  """Calculate the quantity of the specified currency value.
-    Example) $20.00, DenominationValues.FIVE = 4
+def CalcDenominationQuantity(dollarAmount: float, currencyValue: DenominationValues ) -> int:
+  '''Calculate the quantity of the specified currency value. 
   
-    Paremeters:
-    dollarAmount (float): The dollar amount to divide
-    currencyValue: (CurrencyValue): The currency to divide by
+    **Example:** $20.00, DenominationValues.FIVE = 4.
+  
+    **Paremeters:**
+      dollarAmount (float): The dollar amount to divide
+      currencyValue (CurrencyValue): The currency to divide by
 
-    Returns:
-    int: Amount of currency that will fit in the dollarAmount
-  """
+    **Returns:**
+      int: Amount of currency that will fit in the dollarAmount
+  '''
   return floor()
 
 
 
-def CalculateExactChange(dollarAmount: float) :
-  """This function will calculate the exact amount of denominations to return for the dollarAmount.
+def CalculateExactChange(dollarAmount: float) -> dict:
+  '''This function will calculate the exact amount of denominations to return for the dollarAmount.
 
-  Parameters:
+  **Parameters:**
     dollarAmount (float): The dollar amount (2 decimal places) to calculate denominations for. 
   
+  **Returns:** 
+    dict: dictionary of denominations and the amount of each to equal the dollarAmount. 
+  '''
+
   
-  Returns: dictionary of denominations and the amount of each to equal the dollarAmount.
-  """
+
+
   return 
