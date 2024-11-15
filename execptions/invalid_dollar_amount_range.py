755cd049825cc1d141402b3f2af8b4f2c6c5f989
Carson Shively <cshively.business@gmail.com>
2024-11-14 19:32:12 -0600

    created a custom exception for dollarAmount < 0

---
+++ execptions/invalid_dollar_amount_range.py
@@ -0,0 +1,9 @@
+
+
+class InvalidDollarAmountRange(Exception):
+  '''Invalid Dollar Amount Range Exception. This should be thrown when the dollar amount is < 0.'''
+
+  def __init__(self, message):
+    self.message = message
+    super().__init__(self.message)
+  pass
