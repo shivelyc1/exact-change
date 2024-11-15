714dc98cb320452eed672398dacbb01423802759
Carson Shively <cshively.business@gmail.com>
2024-11-14 18:11:15 -0600

    Set up methods for calcluations and added doc comments

---
+++ exact_change.py
@@ -0,0 +1,28 @@
+from Enum.denomination_values import DenominationValues
+from math import floor
+
+def CalcDenominationQuantity(dollarAmount: float, currencyValue: DenominationValues ):
+  """Calculate the quantity of the specified currency value.
+    Example) $20.00, DenominationValues.FIVE = 4
+  
+    Paremeters:
+    dollarAmount (float): The dollar amount to divide
+    currencyValue: (CurrencyValue): The currency to divide by
+
+    Returns:
+    int: Amount of currency that will fit in the dollarAmount
+  """
+  return floor()
+
+
+
+def CalculateExactChange(dollarAmount: float) :
+  """This function will calculate the exact amount of denominations to return for the dollarAmount.
+
+  Parameters:
+    dollarAmount (float): The dollar amount (2 decimal places) to calculate denominations for. 
+  
+  
+  Returns: dictionary of denominations and the amount of each to equal the dollarAmount.
+  """
+  return 
