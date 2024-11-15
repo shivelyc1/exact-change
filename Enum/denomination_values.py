1985c55b50d692ad89694eea0b7059a2fb780f4c
Carson Shively <cshively.business@gmail.com>
2024-11-14 18:12:10 -0600

    denomination enum for values

---
+++ Enum/denomination_values.py
@@ -0,0 +1,21 @@
+'''
+This enumerator is USD Denomination Values in dollars.
+It provides an easy way to access a denomination value with a consistent and more readable format.
+This should help make the code more readable when using these values.
+''' 
+
+from enum import Enum
+
+class DenominationValues(Enum):
+  '''This enumerator is USD Denomination Values in dollars.'''
+  PENNY = 0.01
+  NICKEL = 0.05
+  DIME = 0.10
+  QUARTER = 0.25
+  DOLLAR = 1.00
+  FIVE = 5.00
+  TEN = 10.00
+  TWENTY = 20.00
+  FIFTY = 50.00
+  HUNDRED = 100.00
+
